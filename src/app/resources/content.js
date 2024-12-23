c2a1f2b5af60bd3914056a29bbfcc73c70dfdcee
Mohamed Sami Mazari <124626164+samiMazari@users.noreply.github.com>
2024-12-23 11:28:24 +0100

    Update content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -6,10 +6,10 @@ const person = {
     get name() {
         return `${this.firstName} ${this.lastName}`;
     },
-    role:      'Design Engineer',
+    role:      'Data Science Student',
     avatar:    '/images/avatar.jpg',
-    location:  'Asia/Jakarta',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-    languages: ['English', 'Bahasa']  // optional: Leave the array empty if you don't want to display languages
+    location:  'Europe/Paris',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+    languages: ['English', 'French','Arabic']  // optional: Leave the array empty if you don't want to display languages
 }
 
 const newsletter = {
