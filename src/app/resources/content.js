a25ced2c25528fd719c24c2f6c9cf32311634a86
Mohamed Sami Mazari <124626164+samiMazari@users.noreply.github.com>
2024-12-31 12:31:45 +0100

    Update content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -32,8 +32,8 @@ const social = [
         link: 'https://www.linkedin.com/in/mohamed-sami-mazari/',
     },
     {
-        name: 'Kaggle',
-        icon: 'x',
+        name: 'Instagram',
+        icon: 'instagram',
         link: 'https://www.kaggle.com/mohamedsami01',
     },
     {
