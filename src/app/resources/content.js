e4dd58f02b13ec64ad47ea29bc5f0b37668bad25
Mohamed Sami Mazari <124626164+samiMazari@users.noreply.github.com>
2024-12-31 13:19:02 +0100

    Update content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -188,13 +188,13 @@ const about = {
     }
 }
 
-// const blog = {
-//     label: 'Blog',
-//     title: 'Writing about design and tech...',
-//     description: `Read what ${person.name} has been up to recently`
-//     // Create new blog posts by adding a new .mdx file to app/blog/posts
-//     // All posts will be listed on the /blog route
-// }
+const blog = {
+    label: 'Blog',
+    title: 'Writing about design and tech...',
+    description: `Read what ${person.name} has been up to recently`
+    // Create new blog posts by adding a new .mdx file to app/blog/posts
+    // All posts will be listed on the /blog route
+}
 
 const work = {
     label: 'Work',
