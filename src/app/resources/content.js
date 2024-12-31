a16ee170090a40e6d12aa0ac6d0b3229057267d7
Mohamed Sami Mazari <124626164+samiMazari@users.noreply.github.com>
2024-12-31 14:24:01 +0100

    Update content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -146,12 +146,12 @@ const about = {
             }
         ]
     },
-studies: {
+internship: {
         display: true, // set to false to hide this section
         title: 'Studies internship',
         institutions: [
             {
-                name: 'National School of Statistics and Applied Economics (ENSEA)',
+                name: 'National School of Statistics and Applied Economics (ENSSEA)',
                 description: <>Studied online marketing and personal branding.</>,
             }
         ]
