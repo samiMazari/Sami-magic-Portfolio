d24be5cb0eb400839e56972aa96ed62d64790324
Mohamed Sami Mazari <124626164+samiMazari@users.noreply.github.com>
2025-01-03 14:26:10 +0100

    Update CodeBlock.tsx

--- src/once-ui/modules/code/CodeBlock.tsx
+++ src/once-ui/modules/code/CodeBlock.tsx
@@ -2,6 +2,20 @@
 
 import React, { useState, useEffect, useRef, ReactNode } from 'react';
 
+const KaggleIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    {...props}
+  >
+    {kaggle.svg}
+    <path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm0 18c-4.418 0-8-3.582-8-8s3.582-8 8-8 8 3.582 8 8-3.582 8-8 8z" />
+  </svg>
+);
+
+export default KaggleIcon;
+
 import '@/once-ui/modules/code/CodeHighlight.css';
 import styles from '@/once-ui/modules/code/CodeBlock.module.scss';
 
